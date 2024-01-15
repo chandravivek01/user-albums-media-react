2285a328ed3906aa60cbe7c731ec4d7a68ddbe0a
Vivek Chandra Sardar <chandravivek52@gmail.com>
2024-01-15 07:48:55 +0530

    fetch data via thunks, style loader and display error message ...

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
+
 function UsersList() {
 
-    return 'User list';
+    const dispatch = useDispatch(); 
+    const { isLoading, data, error } = useSelector((state) => {
+        return state.users;
+    });
+
+    useEffect( () => {
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
+    if ( isLoading )
+        return (<Skeleton times={5} className="h-10 w-full" />);
+    
+    if ( error )
+        return (<div>Error fetching data ...</div>);
+
+    return (
+        <div>{data.length}</div>
+    );
 }
 
 export default UsersList;
---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+
+    const response = await axios.get('http://localhost:3005/users');
+    // DEV only!!
+    await pause(5000);
+    return response.data;
+});
+
+// DEV only!!
+const pause = (duration) => {
+    return new Promise( (resolve) => {
+        setTimeout(resolve, duration);
+    })
+}
+
+export { fetchUsers };
